df50f136e96fa5b13fbc61e9f294e76b54441cc8
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-04-16 13:25:22 -0500

    Se agrega funcion para agregar heroes

--- src/app/add-hero/add-hero.component.ts
+++ src/app/add-hero/add-hero.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-add-hero',
@@ -11,14 +12,30 @@ export class AddHeroComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  add(nameHero: String) {}
+  add(nameHero: String, imageHero: String, image_preview, span_preview) {
+    let image = imageHero.substring(
+      imageHero.lastIndexOf('\\') + 1,
+      imageHero.length
+    );
+    image = '../../assets/images/' + image;
+    let name = nameHero.trim();
+    if (!name || !image) {
+      alert('fill a field');
+      return;
+    }
+    this.heroService.addHero({ name, image } as Hero).subscribe((hero) => {
+      alert('add Hero: ' + hero.name);
+    });
+
+    span_preview.style.display = null;
+    image_preview.style.display = null;
+    image_preview.setAttribute('src', '');
+  }
 
   upHeroImage(event, image_preview, span_preview) {
     var self = this;
     const previewImage = image_preview;
     const previewDefaultText = span_preview;
-    console.log(previewImage);
-    console.log(previewDefaultText);
     const file = event.target.files[0];
     if (file) {
       const reader = new FileReader();
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -34,6 +34,13 @@ export class HeroService {
     );
   }
 
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
   updateHero(hero: Hero): Observable<any> {
     return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
       tap((_) => this.log(`updated hero id=${hero.id}`)),
